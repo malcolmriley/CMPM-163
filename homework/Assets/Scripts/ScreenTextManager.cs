a4f4743b12ea3b06c99626aada3119e1ffb7d736
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-22 22:33:48 -0700

    Partial implementation of screen text

--- homework/Assets/Scripts/ScreenTextManager.cs
+++ homework/Assets/Scripts/ScreenTextManager.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenTextManager : MonoBehaviour {
+
+	// Public Fields
+	public Text textElement;
+	public TerminalScreen screen;
+
 	void Start() {
-		
+		SetParameterSfrom(screen);
 	}
 
 	void Update() {
 		
+	}
+
+	private void SetParameterSfrom(TerminalScreen terminalScreen) {
+		textElement.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, terminalScreen.screenWidth);
+		textElement.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, terminalScreen.screenHeight);
+		textElement.text = terminalScreen.textAsset.text;
 	}
 }
---
+++ homework/Assets/Scripts/TerminalScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TerminalScreen", menuName = "DataContainers/TerminalScreen")]
+public class TerminalScreen : ScriptableObject {
+	public TextAsset textAsset;
+	[Range(0, 480)]
+	public int screenWidth = 400;
+	[Range(0, 320)]
+	public int screenHeight = 320;
+}
