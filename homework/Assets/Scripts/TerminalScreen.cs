7299fb614f6ec6a2b7593bba809f5e5db0db1bfd
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-23 01:41:06 -0700

    Fix slow load missing first line

--- homework/Assets/Scripts/TerminalScreen.cs
+++ homework/Assets/Scripts/TerminalScreen.cs
@@ -37,7 +37,7 @@ public class TerminalScreen : ScriptableObject {
 		if (loadSpeed < 1.0F) {
 			_builder = new StringBuilder();
 			_lines = textAsset.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-			_builder.Append(_lines[0]);
+			_builder.AppendLine(_lines[0]);
 		}
 		else {
 			manager.SetScreenText(textAsset.text);
