b44cd1f4fd634c507aeaf421fc4cd15c490837aa
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-23 01:36:57 -0700

    Add load screen to computer

---
+++ homework/Assets/Scripts/TerminalManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameTerminal;
+
+public class TerminalManager : MonoBehaviour {
+
+	// Public Fields
+	public Text textElement;
+	public TerminalScreen loadScreen;
+
+	// Internal Fields
+	private TerminalScreen _screen;
+	private TerminalScreen _previous;
+	private Dictionary<KeyCode, TerminalInteraction> _inputMap = new Dictionary<KeyCode, TerminalInteraction>();
+
+	void Start() {
+		_inputMap[KeyCode.Return] = TerminalInteraction.SELECT;
+		_inputMap[KeyCode.W] = TerminalInteraction.UP;
+		_inputMap[KeyCode.S] = TerminalInteraction.DOWN;
+		_inputMap[KeyCode.A] = TerminalInteraction.LEFT;
+		_inputMap[KeyCode.D] = TerminalInteraction.RIGHT;
+		_inputMap[KeyCode.Space] = TerminalInteraction.BACK;
+		Boot();
+	}
+
+	void Update() {
+		_screen?.OnScreenUpdate(this);
+		HandleInteractions(_screen);
+	}
+
+	public void SwitchToScreen(TerminalScreen newScreen, bool canReturn) {
+		_screen?.OnScreenExit(this);
+		SetParametersfrom(newScreen);
+		newScreen.OnScreenLoad(this);
+		_previous = canReturn ? _screen : null;
+		_screen = newScreen;
+	}
+
+	public void SetScreenText(string passedText) {
+		textElement.text = passedText;
+	}
+
+	public void ReturnToPrevious() {
+		if (!(_previous is null)) {
+			SwitchToScreen(_previous, false);
+		}
+	}
+
+	public void Boot() {
+		SwitchToScreen(loadScreen, false);
+	}
+
+	public Canvas GetCanvas() {
+		return textElement.canvas;
+	}
+
+	// Internal Methods
+	private void HandleInteractions(TerminalScreen behavior) {
+		if (Input.anyKeyDown) {
+			behavior.OnInteract(this, TerminalInteraction.ANY);
+		}
+		else foreach (KeyValuePair<KeyCode, TerminalInteraction> entry in _inputMap) {
+			if (Input.GetKeyDown(entry.Key)) {
+				behavior.OnInteract(this, entry.Value);
+				return;
+			}
+		}
+	}
+
+	private void SetParametersfrom(TerminalScreen terminalScreen) {
+		textElement.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, terminalScreen.screenWidth);
+		textElement.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, terminalScreen.screenHeight);
+		textElement.alignment = terminalScreen.anchor;
+	}
+}
--- homework/Assets/Scripts/TerminalScreen.cs
+++ homework/Assets/Scripts/TerminalScreen.cs
@@ -1,12 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using GameTerminal;
+using System.Text;
+using System;
 
 [CreateAssetMenu(fileName = "TerminalScreen", menuName = "DataContainers/TerminalScreen")]
 public class TerminalScreen : ScriptableObject {
+	// Public Constants
+	public const int WIDTH_DEFAULT = 400;
+	public const int HEIGHT_DEFAULT = 320;
+
+	// Public References
+	public TextAnchor anchor = TextAnchor.UpperLeft;
 	public TextAsset textAsset;
 	[Range(0, 480)]
-	public int screenWidth = 400;
+	public int screenWidth = WIDTH_DEFAULT;
 	[Range(0, 320)]
-	public int screenHeight = 320;
+	public int screenHeight = HEIGHT_DEFAULT;
+	[Range(0.01F, 1.0F)]
+	public float loadSpeed = 0.05F;
+
+	// Internal Objects
+	[NonSerialized]
+	private bool _loaded = false;
+	[NonSerialized]
+	private StringBuilder _builder = null;
+	[NonSerialized]
+	private float _loadProgress = 0.0F;
+	[NonSerialized]
+	private int _lineProgress = 0;
+	[NonSerialized]
+	private string[] _lines = null;
+	[NonSerialized]
+	protected bool _completed = false;
+
+	public virtual void OnScreenLoad(TerminalManager manager) {
+		if (loadSpeed < 1.0F) {
+			_builder = new StringBuilder();
+			_lines = textAsset.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+		}
+		else {
+			manager.SetScreenText(textAsset.text);
+			_completed = true;
+		}
+		_loaded = true;
+	}
+
+	public virtual void OnScreenExit(TerminalManager manager) {
+		_builder?.Clear();
+	}
+
+	public virtual void OnScreenUpdate(TerminalManager manager) {
+		if (_loaded && !_completed) {
+			_loadProgress += loadSpeed;
+			if (_loadProgress >= 1.0F) {
+				_lineProgress += 1;
+				_loadProgress = 0.0F;
+				if (_lineProgress < _lines.Length) {
+					_builder.AppendLine(_lines[_lineProgress]);
+					manager.SetScreenText(_builder.ToString());
+				}
+				else {
+					_completed = true;
+				}
+			}
+		}
+	}
+
+	public virtual void OnInteract(TerminalManager manager, TerminalInteraction interaction) {
+		if (interaction == TerminalInteraction.BACK) {
+			manager.ReturnToPrevious();
+		}
+	}
+}
+
+namespace GameTerminal {
+	public enum TerminalInteraction {
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT,
+		BACK,
+		SELECT,
+		ANY,
+	}
 }
