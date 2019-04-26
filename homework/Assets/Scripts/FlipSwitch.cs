16d08069f9d46a6a292e063d2fa44647c763804b
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-26 15:26:42 -0700

    Add lightswitch functionality

---
+++ homework/Assets/Scripts/FlipSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSwitch : MonoBehaviour {
+
+	// Public Fields
+	public GameObject switchObject;
+	public float angleOffset;
+
+	// Internal Fields
+	private float _restPoint;
+	private bool _enabled;
+
+	void Start() {
+		_restPoint = switchObject.transform.localRotation.x;
+		ToggleSwitch();
+	}
+
+	public void ToggleSwitch() {
+		float direction = 1.0F;
+		_enabled = !_enabled;
+		if (_enabled) {
+			direction *= -1; 
+		}
+		switchObject.transform.localRotation = Quaternion.Euler(_restPoint + angleOffset * direction, 0.0F, 0.0F);
+	}
+}
---
+++ homework/Assets/Scripts/LightController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+	// Public Fields
+	public Light managedLight;
+	public MeshRenderer managedRenderer;
+
+	// Internal Fields;
+	private bool _enabled;
+	private const string EMISSION = "_EmissionColor";
+	private Color _emissionColor;
+
+	public void Start() {
+		_emissionColor = managedLight.color;
+		SetLights(_enabled);
+	}
+
+	public void ToggleLights() {
+		SetLights(!_enabled);
+	}
+
+	public void SetLights(bool activated) {
+		_enabled = activated;
+		managedLight.enabled = activated;
+		if (activated) {
+			managedRenderer.material.SetColor(EMISSION, _emissionColor);
+		}
+		else {
+			managedRenderer.material.SetColor(EMISSION, Color.black);
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
