3d05c3ca21096f05b91c5b89afe432fd525a9652
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-26 16:03:59 -0700

    Improve crystal projection

---
+++ homework/Assets/Scripts/ColorModulator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class ColorModulator : MonoBehaviour {
+
+	// Public Fields
+	public Gradient colorGrade;
+	[Range(0.01F, 3.0F)]
+	public float timeScale;
+
+	// Internal Fields
+	private Light _light;
+
+	public void Start() {
+		_light = GetComponent<Light>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float grade = 0.5F * (1 + Mathf.Sin(timeScale * Time.timeSinceLevelLoad));
+		_light.color = colorGrade.Evaluate(grade);
+	}
+}
---
+++ homework/Assets/Scripts/Hover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+	// Public Fields
+	[Range(0.01F, 5.0F)]
+	public float intensity = 2.0F;
+
+	// Private fields
+	private Vector3 _position;
+
+	// Start is called before the first frame update
+	void Start() {
+		_position = transform.localPosition;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float offset = intensity * Mathf.Sin(Time.timeSinceLevelLoad);
+		transform.localPosition = new Vector3(_position.x, _position.y + offset, _position.z);
+	}
+}
