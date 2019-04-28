91350431326d1233e355e2c03dbf23f57ce4500c
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-27 23:21:22 -0700

    Create MenuItem prefab and associated elements

---
+++ homework/Assets/Scripts/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MenuItem", menuName = "DataContainers/MenuItem")]
+public class MenuItem : ScriptableObject {
+
+	// Public References
+	public GameObject screenPrefab;
+	public Sprite itemIcon;
+	public string itemName;
+	public string itemType;
+}
---
+++ homework/Assets/Scripts/MenuItemController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class MenuItemController : MonoBehaviour {
+
+	// Public References
+	public Image fileIcon;
+	public Text filename;
+	public Text filetype;
+
+	public void SetIcon(Sprite newIcon) {
+		fileIcon.sprite = newIcon;
+	}
+
+	public void SetFilename(string newName) {
+		filename.text = newName;
+	}
+
+	public void SetFiletype(string newType) {
+		filetype.text = newType;
+	}
+
+	public void SetSelected(bool value) {
+		GetComponent<Image>().enabled = value;
+	}
+}
