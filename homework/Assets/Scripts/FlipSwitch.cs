1c3b324fa5d00cb2be2094faba7ccdb7f34b0217
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-04-28 16:06:51 -0700

    Add sound assistance scripts

--- homework/Assets/Scripts/FlipSwitch.cs
+++ homework/Assets/Scripts/FlipSwitch.cs
@@ -8,21 +8,36 @@ public class FlipSwitch : MonoBehaviour {
 	public GameObject switchObject;
 	public float angleOffset;
 
+	public AudioSource source;
+	public AudioClip onSound;
+	public AudioClip offSound;
+
 	// Internal Fields
 	private float _restPoint;
 	private bool _enabled;
 
 	void Start() {
 		_restPoint = switchObject.transform.localRotation.x;
-		ToggleSwitch();
+		HandleTransform();
 	}
 
 	public void ToggleSwitch() {
+		HandleTransform();
+		PlaySound();
+	}
+
+	// Internal Methods
+	private void HandleTransform() {
 		float direction = 1.0F;
 		_enabled = !_enabled;
 		if (_enabled) {
-			direction *= -1; 
+			direction *= -1;
 		}
 		switchObject.transform.localRotation = Quaternion.Euler(_restPoint + angleOffset * direction, 0.0F, 0.0F);
+	}
+
+	private void PlaySound() {
+		AudioClip sound = _enabled ? onSound : offSound;
+		source.PlayOneShot(sound);
 	}
 }
---
+++ homework/Assets/Scripts/RandomSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomSound : MonoBehaviour {
+
+	// Public Fields
+	public List<AudioClip> sounds;
+
+	// Internal Fields
+	private AudioSource _source;
+
+	public void Start() {
+		_source = GetComponent<AudioSource>();
+	}
+
+	public void PlayRandom() {
+		int index = Random.Range(0, sounds.Count);
+		_source.PlayOneShot(sounds[index]);
+	}
+}
