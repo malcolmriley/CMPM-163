4f2e20ea016e3cf3954564ae086b08fb523ad7fb
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-08 20:18:55 -0700

    Implement Animation from fbx

---
+++ homework/Assets/Scripts/CycleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class CycleController : MonoBehaviour {
+
+	// Private References
+	private Animator _animator;
+	private Animation _animation;
+
+	void Start() {
+		_animator = GetComponent<Animator>();
+		_animation = GetComponent<Animation>();
+	}
+
+	void Update() {
+		
+	}
+}
