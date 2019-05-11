72a8cc8db85f23d8fda4d04e7c180c9cd0d48ad5
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-11 16:16:15 -0700

    Add color randomization to cycle

--- homework/Assets/Scripts/CycleController.cs
+++ homework/Assets/Scripts/CycleController.cs
@@ -7,6 +7,7 @@ public class CycleController : MonoBehaviour {
 
 	// Public References
 	public ParticleSystem particles;
+	public Material armorMaterial;
 
 	// Private References
 	private Animator _animator;
@@ -18,10 +19,26 @@ public class CycleController : MonoBehaviour {
 	}
 
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.Space)) {
+		if (Input.GetKeyDown(KeyCode.Space) && !_animation.isPlaying) {
 			_animation.Play();
+			// Initialize New Color
+			Color newColor = GetColor();
+
+			// Set Particle Parameters
+			var module = particles.colorOverLifetime;
+			module.color = newColor;
+
+			// Set Armor Color
+			armorMaterial.color = newColor;
+
 			particles.Stop();
 			particles.Play();
 		}
+	}
+
+	// Internal Methods
+
+	private Color GetColor() {
+		return Random.ColorHSV(0.0F, 1.0F, 0.85F, 1.0F, 0.4F, 0.8F);
 	}
 }
