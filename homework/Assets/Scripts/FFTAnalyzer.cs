3ef81613fe3cb46d7796368bb5d99eba9fe0215f
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-18 15:57:57 -0700

    Implement FFTAnalyzer script

---
+++ homework/Assets/Scripts/FFTAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioListener))]
+public class FFTAnalyzer : MonoBehaviour {
+
+	// Public Fields
+	public FFTWindow windowType;
+	public int sampleSize;
+	public float[] Spectrum { get; private set; }
+
+	// Internal Methods
+	private AudioListener _listener;
+
+	public void Start() {
+		_listener = GetComponent<AudioListener>();
+		Spectrum = new float[Mathf.ClosestPowerOfTwo(sampleSize)];
+	}
+
+	public void Update() {
+		AudioListener.GetSpectrumData(Spectrum, 0, windowType);
+	}
+}
