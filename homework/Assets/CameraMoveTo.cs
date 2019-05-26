60861a8a96a182c253b179d74dc174106b8f4247
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-26 12:07:45 -0700

    Add camera move over time

---
+++ homework/Assets/CameraMoveTo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveTo : MonoBehaviour {
+
+	// Public Fields
+	public Transform moveTarget;
+	public float moveTime = 15.0F;
+
+	// Internal Fields
+	private Vector3 _startPosition;
+	private float _progress;
+
+	public void Start() {
+		_startPosition = transform.localPosition;
+	}
+
+	public void Update() {
+		_progress = Mathf.Clamp01(_progress + (Time.deltaTime / moveTime));
+		transform.localPosition = Vector3.Lerp(_startPosition, moveTarget.localPosition, _progress);
+	}
+}
