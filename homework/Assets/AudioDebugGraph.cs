567993327edae3bfaa45dd8d30d04d790273d6e5
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-26 13:32:32 -0700

    Add audio spectrum debug line

---
+++ homework/Assets/AudioDebugGraph.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class AudioDebugGraph : MonoBehaviour {
+
+	// Public Fields
+	public FFTAnalyzer analyzer;
+	public Vector2 heightRange;
+	public AnimationCurve normalizer;
+	public GameObject barPrefab;
+	public Gradient colorGrade;
+
+	// Internal Methods
+	private List<GameObject> _managedObjects;
+	private Vector2 _dimensions;
+
+	void Start() {
+		_dimensions = GetComponent<RectTransform>().sizeDelta;
+		int quantity = analyzer.Spectrum.Length;
+		_managedObjects = new List<GameObject>(quantity);
+		for (int count = 0; count < quantity; count += 1) {
+			GameObject instance = Instantiate(barPrefab, transform);
+			_managedObjects.Add(instance);
+			RectTransform rect = instance.GetComponent<RectTransform>();
+			if (rect != null) {
+				rect.sizeDelta = new Vector2(_dimensions.x / quantity, heightRange.x);
+			}
+		}
+	}
+
+	void Update() {
+		for (int index = 0; index < _managedObjects.Count; index += 1) {
+			float value = Mathf.Clamp01(analyzer.Spectrum[index] * normalizer.Evaluate((float)index / (float)analyzer.Spectrum.Length)); 
+
+			// Set Bar Height
+			RectTransform rect = _managedObjects[index].GetComponent<RectTransform>();
+			if (rect != null) {
+				rect.sizeDelta = new Vector2(rect.sizeDelta.x, Mathf.Lerp(heightRange.x, heightRange.y, value));
+			}
+
+			// Set Bar Color
+			Image image = _managedObjects[index].GetComponent<Image>();
+			if (image != null) {
+				image.color = colorGrade.Evaluate(value);
+			}
+		}
+	}
+}
--- homework/Assets/Scripts/VFXSystemController.cs
+++ homework/Assets/Scripts/VFXSystemController.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class VFXSystemController : MonoBehaviour {
 
 	// Public Fields
+	[Header("FFT Analyzer Properties")]
 	public FFTAnalyzer analyzer;
+	public Vector2 bassRange;
+	public Vector2 midRange;
+	public Vector2 trebleRange;
+
+	[Header("Output Modulation")]
 	public Gradient lightColorGrade;
 
 	[Header("Internal Use Only")]
@@ -21,6 +28,33 @@ public class VFXSystemController : MonoBehaviour {
 	}
 
 	void Update() {
-		
+		sigilSprite.color = lightColorGrade.Evaluate(GetTreble());
+	}
+
+	// Internal Methods
+	private float GetBass() {
+		return AverageRange(analyzer.Spectrum, bassRange);
+	}
+
+	private float GetMidrange() {
+		return AverageRange(analyzer.Spectrum, midRange);
+	}
+
+	private float GetTreble() {
+		return AverageRange(analyzer.Spectrum, trebleRange);
+	}
+
+	private float AverageRange(float[] array, Vector2 range) {
+		int min = Mathf.RoundToInt(range.x * (array.Length - 1));
+		int max = Mathf.RoundToInt(range.y * (array.Length - 1));
+
+		if (max < min) return 0;
+		if (max == min) return array[max];
+
+		float average = 0.0F;
+		for (int index = min; index <= max; index += 1) {
+			average += array[index];
+		}
+		return (average) / (max - min + 1);
 	}
 }
