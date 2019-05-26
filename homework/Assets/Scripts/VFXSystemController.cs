a8c96d0ee274f2aeb7a0f7a52d636fd52f16edc2
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-25 19:20:03 -0700

    Set up VFX Skeleton

---
+++ homework/Assets/Scripts/VFXSystemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXSystemController : MonoBehaviour {
+
+	// Public Fields
+	public FFTAnalyzer analyzer;
+
+	[Header("Internal Use Only")]
+	public Light light;
+	public ParticleSystem smokeSystem;
+	public ParticleSystem glowSystem;
+	public ParticleSystem electricSystem;
+	public ParticleSystem sigilSystem;
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+}
