eec74727195659b4e9371ca4f1c0b95bb4a5764a
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-05-26 02:29:40 -0700

    Implement noise-driven camera drift

---
+++ homework/Assets/CameraDrift.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrift : MonoBehaviour {
+
+	// Public Fields
+	public float maxPitch = 2.0F;
+	public float maxYaw = 2.0F;
+	public float timeScale = 0.1F;
+
+	// Internal Fields
+	private Quaternion _startRotation;
+
+	void Start() {
+		_startRotation = transform.rotation;
+	}
+
+	void Update() {
+		float pitch = GetXNoiseModulation(maxPitch);
+		float yaw = GetYNoiseModulation(maxYaw);
+		transform.rotation = _startRotation * Quaternion.Euler(0.0F, pitch, yaw);
+	}
+
+	// Internal Methods
+	private float GetXNoiseModulation(float value) {
+		return GetNoiseModulation(value, GetScaledTime(), 0.0F);
+	}
+
+	private float GetYNoiseModulation(float value) {
+		return GetNoiseModulation(value, 0.0F, GetScaledTime());
+	}
+
+	private float GetNoiseModulation(float value, float xParam, float yParam) {
+		return value * (0.5F - Mathf.PerlinNoise(xParam, yParam)) * 2.0F;
+	}
+
+	private float GetScaledTime() {
+		return Time.timeSinceLevelLoad * timeScale;
+	}
+}
